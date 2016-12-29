1e795af4e1f625cbd077940e9c65795daad47062
Mihailo Gajic <mgajic@goodgamestudios.com>
2016-12-29 15:46:18 +0100

    ci workflow updated

--- phpunit.sh
+++ phpunit.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 docker-compose build \
-    && docker rm -f mq-web \
+    && docker-compose stop \
+    && docker-compose rm -f \
     && docker-compose up -d \
     && docker-compose exec mq-web ./vendor/bin/phpunit
