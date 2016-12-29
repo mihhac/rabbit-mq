f22db56010dfe22ba7dfb530da9c97fde562d06e
Mihailo Gajic <mgajic@goodgamestudios.com>
2016-12-29 15:59:53 +0100

    phpunit.sh was updated to support rabbitmq to get up

--- phpunit.sh
+++ phpunit.sh
@@ -4,4 +4,5 @@ docker-compose build \
     && docker-compose stop \
     && docker-compose rm -f \
     && docker-compose up -d \
+    && sleep 5 \
     && docker-compose exec mq-web ./vendor/bin/phpunit
