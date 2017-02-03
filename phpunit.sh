9d8725bab0ec1c3927c74e093d74875de95222e6
Mihac <finder_ausberlin@yahoo.de>
2017-02-03 22:05:12 +0100

    docker build, remove old image

--- phpunit.sh
+++ phpunit.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
-docker-compose build \
-    && docker-compose stop \
+IMAGE_NAME=brofist/mq-web:latest
+
+docker-compose stop \
     && docker-compose rm -f \
+
+if [ ! -z $(docker images -q $IMAGE_NAME) ]
+then
+    echo "Removing previous image $IMAGE_NAME ..."
+    docker rmi $IMAGE_NAME
+    echo "Image $IMAGE_NAME is removed ..."
+else
+    echo "Image doesn't exist"
+fi
+
+docker-compose build \
     && docker-compose up -d \
-    && sleep 5 \
+    && sleep 20 \
     && docker-compose exec -T mq-web ./vendor/bin/phpunit \
 
 #or simply use docker exec
